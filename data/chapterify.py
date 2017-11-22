10af5465cc22c90181f2deb5c5520ab7c52e313c
bddicken <bddicken@gmail.com>
2017-11-22 12:30:59 -0700

    Add comments to chapterify.py

--- data/chapterify.py
+++ data/chapterify.py
@@ -1,3 +1,12 @@
+
+### Author: Benjamin Dicken
+### Description:
+### This script is used to pre-process a raw Bible text file, before being used by wordflow.
+### It works on a file that is formatted like the asv-full.txt file, which can be found in the same directory as this script.
+### It already knows to process this file, so no need to provide command-line arguments.
+### In the future, it would be good to get multiple versions of the Bible, and have this be able to process any of them.
+### The output format is JSON, and it saves the output to a file called bible.json.
+
 import os
 import sys
 import json
@@ -33,3 +42,4 @@ for line in f:
 
 with open('bible.json', 'w') as fp:
     json.dump(bible, fp, sort_keys=True, indent=4)
+
